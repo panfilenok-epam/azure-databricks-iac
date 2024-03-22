2b4a516f00d463716960c454b587f892df5f7d3c
Alexander Panfilenok <alexander_panfilenok@epam.com>
2024-03-22 22:42:41 +0100

    initial commit

---
+++ env/dev/backend.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "rg-terraform-state"
+storage_account_name = "stmyterraform1984"
+container_name       = "state"
+key                  = "dev/iac.tfstate"
---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.89.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "1.35.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  use_oidc = true
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-databricks-poc"
+  location = "westeurope"
+}
+
+resource "azurerm_databricks_workspace" "db_workspace" {
+  name                = "my-databricks-workspace"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "premium"
+
+  custom_parameters {
+    no_public_ip = true
+  }
+}
+
+provider "databricks" {
+  alias                       = "azure"
+  host                        = azurerm_databricks_workspace.db_workspace.workspace_url
+  azure_workspace_resource_id = azurerm_databricks_workspace.db_workspace.id
+}
+
+resource "time_sleep" "wait_for_workspace" {
+  create_duration = "30s"
+  depends_on      = [azurerm_databricks_workspace.db_workspace]
+}
+
+resource "databricks_cluster" "db_cluster" {
+  cluster_name            = "my-databricks-cluster"
+  spark_version           = "12.2.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 10
+  autoscale {
+    min_workers = 1
+    max_workers = 1
+  }
+  provider = databricks.azure
+}
